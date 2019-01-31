5f724754a5e7e044b7d75b08e69e9cb4d0d482e2
Alex Johnson <alxmjo@gmail.com>
2019-01-31 15:28:08 -0800

    Integrate Travis with CMake
    Several changes in here, but the crux is that it updates Travis so that Travis pulls the build configuration from CMake (via CMakeLists.txt). This prevents us from having to maintain a build configuration for Travis as well as a build configuration for CMake.

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -1,25 +1,7 @@
-#define private public // for testing private methods
 #define CATCH_CONFIG_MAIN
 
 #include "../include/catch.hpp"
 #include "../source/Board.hpp"
-
-TEST_CASE("Coord and Element Conversion")
-{
-	Board board;
-
-	REQUIRE(board.coordsToElement("a1") == 56);
-	REQUIRE(board.coordsToElement("a8") == 0);
-	REQUIRE(board.coordsToElement("h1") == 63);
-	REQUIRE(board.coordsToElement("h8") == 7);
-	REQUIRE(board.coordsToElement("c6") == 18);
-
-	REQUIRE(board.elementToCoords(56) == "a1");
-	REQUIRE(board.elementToCoords(0) == "a8");
-	REQUIRE(board.elementToCoords(63) == "h1");
-	REQUIRE(board.elementToCoords(7) == "h8");
-	REQUIRE(board.elementToCoords(18) == "c6");
-}
 
 TEST_CASE("Set and Get Pieces")
 {
