a2846a29c205754549089acd6ae63a355a4f44a0
Alex Johnson <alxmjo@gmail.com>
2019-02-06 16:09:40 -0800

    Add all pawns and print board properly
    - Allow printing of board in standard 8 x 8 arrangement

--- source/Board.cpp
+++ source/Board.cpp
@@ -13,15 +13,79 @@ Board::Board()
 /* Squares are stored in a map which maps algebraic locations (as a string) to a unique_ptr to a Square object. */
 void Board::prepSquares()
 {
-	// TODO: This is only partway filled. We could either list these out (over 64 lines) or create a loop which
-	//		creates these dynamically.
+	// TODO: Wrap these in a loop
 
 	squares.insert(std::make_pair("a1", std::make_unique<Square>()));
-	squares.insert(std::make_pair("b1", std::make_unique<Square>()));
 	squares.insert(std::make_pair("a2", std::make_unique<Square>()));
-	squares.insert(std::make_pair("b2", std::make_unique<Square>()));
 	squares.insert(std::make_pair("a3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("a4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("a5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("a6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("a7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("a8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("b1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b2", std::make_unique<Square>()));
 	squares.insert(std::make_pair("b3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("b8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("c1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("c8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("d1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("d8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("e1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("e8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("f1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("f8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("g1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("g8", std::make_unique<Square>()));
+
+	squares.insert(std::make_pair("h1", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h2", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h3", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h4", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h5", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h6", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h7", std::make_unique<Square>()));
+	squares.insert(std::make_pair("h8", std::make_unique<Square>()));
 }
 
 /* Creates and places the appropriate pieces on the appropriate spots to start a game. */
@@ -29,9 +93,25 @@ void Board::prepPieces()
 {
 	// TODO: Fill out the rest of the pieces.
 
-	// White Pawn Placement
+	// White pawns
 	setPiece("a2", std::make_unique<Pawn>(WHITE));
 	setPiece("b2", std::make_unique<Pawn>(WHITE));
+	setPiece("c2", std::make_unique<Pawn>(WHITE));
+	setPiece("d2", std::make_unique<Pawn>(WHITE));
+	setPiece("e2", std::make_unique<Pawn>(WHITE));
+	setPiece("f2", std::make_unique<Pawn>(WHITE));
+	setPiece("g2", std::make_unique<Pawn>(WHITE));
+	setPiece("h2", std::make_unique<Pawn>(WHITE));
+
+	// Black pawns
+	setPiece("a7", std::make_unique<Pawn>(BLACK));
+	setPiece("b7", std::make_unique<Pawn>(BLACK));
+	setPiece("c7", std::make_unique<Pawn>(BLACK));
+	setPiece("d7", std::make_unique<Pawn>(BLACK));
+	setPiece("e7", std::make_unique<Pawn>(BLACK));
+	setPiece("f7", std::make_unique<Pawn>(BLACK));
+	setPiece("g7", std::make_unique<Pawn>(BLACK));
+	setPiece("h7", std::make_unique<Pawn>(BLACK));
 }
 
 /* Sets a piece to its appropriate location in the map. */
@@ -47,37 +127,50 @@ const Piece* Board::getPiece(const std::string &coords) const
 	return squares.find(coords)->second->getPiece();
 }
 
-/* Print contents of board. */
+/* Print contents of board with black on top and white on bottom by creating a series of coordinates and then finding
+ * the pieces at those coordinates. */
 void Board::printBoard()
 {
-	// TODO: This currently just prints the board as a list of coordinates. Eventually we'll want to turn this into
-	// 		an actual board.
-
-	// Iterate over the map and print each element. If there's a piece there, print the piece.
-	for (const auto &element : squares) {
-
-		if (element.second->getPiece() == nullptr) {
-			std::cout << element.first;
-		}
-		else
+	const std::string letters = "abcdefgh";
+	for (int i = 8; i >= 1; i--) 	// for iterating over numerals 1 through 8
+	{
+		for (int j = 0; j < 8; j++) // for iterating over letters a through g
 		{
-			std::cout << element.first << " " << *element.second->getPiece();
+			std::string coords = letters[j] + std::to_string(i);			// create coordinates
+			const Piece* piece = squares.find(coords)->second->getPiece();	// get piece (or nullptr) at that coordinate
+			if (piece == nullptr) {
+				std::cout << "   ";											// no piece here so print empty space
+			}
+			else
+			{
+				std::cout << *piece;										// print piece
+			}
+			std::cout << " ";												// print spacing to separate pieces
 		}
-		std::cout << std::endl;
+		std::cout << std::endl;												// move to next line
 	}
 }
 
 /* Moves a piece at one coordinate to another coordinate. */
 bool Board::movePiece(const std::string &fromCoords, const std::string &toCoords)
 {
-	// TODO: Implement error checking. If there is no piece at the from location, or if squares don't exist,
-	//  	program will crash.
+	// TODO: Improve error checking by verifying that coordinates are valid.
+
+	std::cout << "Attempting to move from " << fromCoords << " to " << toCoords << std::endl;
 
 	// When the interior setPiece() function is called it sets the piece currently at that location (fromCoords) to
 	// nullptr and then returns a pointer to the piece that was previously at that location. That piece is then assigned
-	// to the square referenced by the parameter toCoords.
-	setPiece(toCoords, setPiece(fromCoords, nullptr));
-	return false;
+	// to the square referenced by the parameter toCoords. Properly handles absence of a piece at a location.
+	if (getPiece(fromCoords) != nullptr)
+	{
+		setPiece(toCoords, setPiece(fromCoords, nullptr));
+		return true;
+	}
+	else
+	{
+		std::cout << "Error: Cannot move from " << fromCoords << " because there's no piece there" << std::endl;
+		return false;
+	}
 }
 
 /* Destructor */
