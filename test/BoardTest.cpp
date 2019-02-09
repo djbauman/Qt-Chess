09863a8b1d68331e1a5505f641db48995229c517
Alex Johnson <alxmjo@gmail.com>
2019-02-08 16:13:36 -0800

    Fix TRUE and true cases in test file

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -53,24 +53,24 @@ TEST_CASE("Clear and obstructed paths")
 	// 		Board, these tests will no longer be accurate.
 
 	// Clear paths
-	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{0,0}) == TRUE); // same location
-	REQUIRE(board.isPathClear(std::pair{1,0}, std::pair{1,1}) == TRUE); // adjacent location
-	REQUIRE(board.isPathClear(std::pair{1,3}, std::pair{6,3}) == TRUE);	// N->S vertical
-	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{0,7}) == TRUE); // W->E horizontal
-	REQUIRE(board.isPathClear(std::pair{2,2}, std::pair{5,5}) == TRUE); // NW->SE diagonal
-	REQUIRE(board.isPathClear(std::pair{5,5}, std::pair{2,2}) == TRUE); // SE->NW diagonal
-	REQUIRE(board.isPathClear(std::pair{6,6}, std::pair{1,1}) == TRUE); // SE->NW diagonal
-	REQUIRE(board.isPathClear(std::pair{1,1}, std::pair{6,6}) == TRUE); // NW->SE diagonal
-	REQUIRE(board.isPathClear(std::pair{6,1}, std::pair{1,6}) == TRUE); // SW->NE diagonal
+	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{0,0}) == true); // same location
+	REQUIRE(board.isPathClear(std::pair{1,0}, std::pair{1,1}) == true); // adjacent location
+	REQUIRE(board.isPathClear(std::pair{1,3}, std::pair{6,3}) == true);	// N->S vertical
+	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{0,7}) == true); // W->E horizontal
+	REQUIRE(board.isPathClear(std::pair{2,2}, std::pair{5,5}) == true); // NW->SE diagonal
+	REQUIRE(board.isPathClear(std::pair{5,5}, std::pair{2,2}) == true); // SE->NW diagonal
+	REQUIRE(board.isPathClear(std::pair{6,6}, std::pair{1,1}) == true); // SE->NW diagonal
+	REQUIRE(board.isPathClear(std::pair{1,1}, std::pair{6,6}) == true); // NW->SE diagonal
+	REQUIRE(board.isPathClear(std::pair{6,1}, std::pair{1,6}) == true); // SW->NE diagonal
 
 	// Obstructed paths
-	REQUIRE(board.isPathClear(std::pair{0,2}, std::pair{2,2}) == FALSE); // N->S vertical
-	REQUIRE(board.isPathClear(std::pair{0,3}, std::pair{7,3}) == FALSE); // N->S vertical
-	REQUIRE(board.isPathClear(std::pair{1,0}, std::pair{1,2}) == FALSE); // W->E horizontal
-	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{7,7}) == FALSE); // NW->SE diagonal
-	REQUIRE(board.isPathClear(std::pair{7,7}, std::pair{0,0}) == FALSE); // SE->NW diagonal
-	REQUIRE(board.isPathClear(std::pair{7,0}, std::pair{0,7}) == FALSE); // SW->NE diagonal
-	REQUIRE(board.isPathClear(std::pair{3,2}, std::pair{4,7}) == FALSE); // not vertical, horizontal, or diagonal
+	REQUIRE(board.isPathClear(std::pair{0,2}, std::pair{2,2}) == false); // N->S vertical
+	REQUIRE(board.isPathClear(std::pair{0,3}, std::pair{7,3}) == false); // N->S vertical
+	REQUIRE(board.isPathClear(std::pair{1,0}, std::pair{1,2}) == false); // W->E horizontal
+	REQUIRE(board.isPathClear(std::pair{0,0}, std::pair{7,7}) == false); // NW->SE diagonal
+	REQUIRE(board.isPathClear(std::pair{7,7}, std::pair{0,0}) == false); // SE->NW diagonal
+	REQUIRE(board.isPathClear(std::pair{7,0}, std::pair{0,7}) == false); // SW->NE diagonal
+	REQUIRE(board.isPathClear(std::pair{3,2}, std::pair{4,7}) == false); // not vertical, horizontal, or diagonal
 }
 
 TEST_CASE()
@@ -80,10 +80,10 @@ TEST_CASE()
 	const Piece *blackPawn = board.getPiece(std::pair{1,7});
 
 	// Forward movement
-	REQUIRE(board.isForwardMove(std::pair{6,7}, std::pair{5,7}, whitePawn) == TRUE);
-	REQUIRE(board.isForwardMove(std::pair{1,7}, std::pair{2,7}, blackPawn) == TRUE);
+	REQUIRE(board.isForwardMove(std::pair{6,7}, std::pair{5,7}, whitePawn) == true);
+	REQUIRE(board.isForwardMove(std::pair{1,7}, std::pair{2,7}, blackPawn) == true);
 
 	// Backward movement
-	REQUIRE(board.isForwardMove(std::pair{6,7}, std::pair{7,7}, whitePawn) == FALSE);
-	REQUIRE(board.isForwardMove(std::pair{1,7}, std::pair{0,7}, blackPawn) == FALSE);
+	REQUIRE(board.isForwardMove(std::pair{6,7}, std::pair{7,7}, whitePawn) == false);
+	REQUIRE(board.isForwardMove(std::pair{1,7}, std::pair{0,7}, blackPawn) == false);
 }
