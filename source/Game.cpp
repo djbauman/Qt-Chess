41c6e4a8bbf5ecc8cb32dbb9917a211e86daa626
Alex Johnson <alxmjo@gmail.com>
2019-02-08 16:47:34 -0800

    Implement isValidMove for Pawn
    - Implement function
    - Add demonstration in Game.cpp

--- source/Game.cpp
+++ source/Game.cpp
@@ -2,7 +2,16 @@
 
 void Game::run()
 {
+	std::cout << "Starting board for Pawn Bowl 2019" << std::endl;
 	board.printBoard();
-	board.movePiece(std::make_pair(6, 0), std::make_pair(6, 1));
+	board.movePiece(std::pair{6,0}, std::pair{4, 0});
 	board.printBoard();
+	board.movePiece(std::pair{1,0}, std::pair{3, 0});
+	board.printBoard();
+	board.movePiece(std::pair{6,1}, std::pair{4, 1});
+	board.printBoard();
+	board.movePiece(std::pair{3,0}, std::pair{4, 1});
+	board.printBoard();
+	std::cout << "Capture!" << std::endl;
+	board.movePiece(std::pair{4,0}, std::pair{4, 1});
 }
--- source/Pawn.cpp
+++ source/Pawn.cpp
@@ -7,13 +7,72 @@ Pawn::Pawn(Color c) : Piece(PAWN, c) {}
 /* Determine if this is a valid move based on the rules of the pawn. */
 bool Pawn::isValidMove(const Board *board, const std::pair<int, int> &fromCoords, const std::pair<int, int> &toCoords) const
 {
-	// TODO: Implement
-	if (board->isOccupied(toCoords))
+	int moveLength = board->getMoveLength(fromCoords, toCoords);
+
+	// Not valid if not forward move
+	if (!board->isForwardMove(fromCoords, toCoords, this))
 	{
 		return false;
 	}
-	else
+
+	// Not valid if horizontal move
+	if (board->isHorizontalMove(fromCoords, toCoords))
 	{
-		return true;
+		return false;
 	}
+
+	// Must be length 1 and capture if diagonal
+	if (board->isDiagonalMove(fromCoords, toCoords))
+	{
+		// Is 1 step?
+		if (moveLength == 1)
+		{
+			if (board->isOccupied(toCoords))
+			{
+				// Capture!
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			// Only moveLength of 1 is allowed on diagonals
+			return false;
+		}
+	}
+
+	// Must be length 1 or 2, clear path, and unoccupied destination if vertical
+	if (board->isVerticalMove(fromCoords, toCoords))
+	{
+		// Is occupied?
+		if (board->isOccupied(toCoords))
+		{
+			return false;
+		}
+
+		// Is length 2?
+		if (moveLength == 2)
+		{
+			if (!moved && board->isPathClear(fromCoords, toCoords))
+			{
+				return true;
+			}
+		}
+		else if (moveLength == 1)
+		{
+			return true;
+		}
+		else
+		{
+			// No lengths besides 1 and 2 permitted
+			return false;
+		}
+	}
+
+	// If we've gotten this far, definitely not a valid move
+	return false;
+
 }
