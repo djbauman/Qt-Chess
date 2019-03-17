ad7294f313b61cae18d7c484533a265ae5a52dfb
Alex Johnson <alxmjo@gmail.com>
2019-03-17 15:35:43 -0700

    Add comments to Square::setPiece()

--- source/Square.cpp
+++ source/Square.cpp
@@ -7,9 +7,15 @@ Piece* Square::getPiece() const
 	return piece.get();
 }
 
+/* Takes a pieces and associates it with a square. Returns the piece that use to occupy the square, if any. */
 std::unique_ptr<Piece> Square::setPiece(std::unique_ptr<Piece> piece)
 {
+	// Get the piece that was previously occupying this square, if any
 	std::unique_ptr<Piece> old = std::move(this->piece);
+
+	// Take the piece that was passed in as an argument and associate it with this square.
 	this->piece = std::move(piece);
+
+	// If the square was previously occupied, this will return unique pointer to that piece, otherwise it returns nullptr
 	return old;
 }
